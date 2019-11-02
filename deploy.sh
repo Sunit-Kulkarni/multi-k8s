7b3f6d5c605de1ad20b003e5b30b5d1c45ed44ce
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2019-11-02 02:26:02 -0400

    replaced docker repo name with sunitkulkarni

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker build -t sunitkulkarni/multi-client:latest -t sunitkulkarni/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sunitkulkarni/multi-server:latest -t sunitkulkarni/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sunitkulkarni/multi-worker:latest -t sunitkulkarni/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push sunitkulkarni/multi-client:latest
+docker push sunitkulkarni/multi-server:latest
+docker push sunitkulkarni/multi-worker:latest
 
-docker push stephengrider/multi-client:latest:$SHA
-docker push stephengrider/multi-server:latest:$SHA
-docker push stephengrider/multi-worker:latest:$SHA
+docker push sunitkulkarni/multi-client:latest:$SHA
+docker push sunitkulkarni/multi-server:latest:$SHA
+docker push sunitkulkarni/multi-worker:latest:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment server=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=sunitkulkarni/multi-server:$SHA
+kubectl set image deployments/client-deployment server=sunitkulkarni/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=sunitkulkarni/multi-worker:$SHA
